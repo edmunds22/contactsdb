41ab39ca0d63fef2a30bff04c05840a8dd74ef31
William Edmunds <wwedmunds@gmail.com>
2019-06-20 14:31:46 +0100

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,17 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
       }
+    }
     ]
   },
 
